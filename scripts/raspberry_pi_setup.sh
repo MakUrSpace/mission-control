aac8829fb9ba886b3bfd22f8534ffb3eb1e87c08
Nate Brandeburg <brandeburgnate@gmail.com>
2023-12-06 22:34:07 -0600

    Add Avahi alias hostname script and systemd service

--- scripts/raspberry_pi_setup.sh
+++ scripts/raspberry_pi_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
 # Check if Avahi is installed
 if ! dpkg -s avahi-daemon avahi-utils >/dev/null 2>&1; then
     echo "Installing Avahi..."
@@ -15,37 +20,70 @@ sudo systemctl enable avahi-daemon
 sudo systemctl start avahi-daemon
 echo "Avahi service enabled and started."
 
+# Path for the alias hostname script
+ALIAS_SCRIPT_PATH="/usr/local/bin/publish_alias.sh"
+
 # Function to create alias hostname script
 create_alias_script() {
-    echo "Creating alias hostname script"
+    echo "Creating alias hostname script at ${ALIAS_SCRIPT_PATH}"
 
-    cat > publish_alias.sh <<EOF
+    sudo tee "${ALIAS_SCRIPT_PATH}" >/dev/null <<'EOF'
 #!/bin/bash
-IP_ADDRESS=\$(hostname -I | cut -d' ' -f1)
+IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
 
-if [ -z "\$IP_ADDRESS" ]; then
+if [ -z "$IP_ADDRESS" ]; then
     echo "Error: IP address not found."
     exit 1
 fi
 
 # Kill previous instances to avoid duplicates
-kill \$(pgrep -f avahi-publish) 2>/dev/null
+kill $(pgrep -f avahi-publish) 2>/dev/null
 
 # Publish alias hostnames
-avahi-publish -a mainsail.local -R \$IP_ADDRESS >/dev/null 2>&1 &
-avahi-publish -a missioncontrol.local -R \$IP_ADDRESS >/dev/null 2>&1 &
-avahi-publish -a octoprint.local -R \$IP_ADDRESS >/dev/null 2>&1 &
-avahi-publish -a traefik.local -R \$IP_ADDRESS >/dev/null 2>&1 &
+avahi-publish -a mainsail.local -R $IP_ADDRESS >/dev/null 2>&1 &
+avahi-publish -a missioncontrol.local -R $IP_ADDRESS >/dev/null 2>&1 &
+avahi-publish -a octoprint.local -R $IP_ADDRESS >/dev/null 2>&1 &
+avahi-publish -a traefik.local -R $IP_ADDRESS >/dev/null 2>&1 &
 EOF
 
-    chmod +x publish_alias.sh
+    sudo chmod +x "${ALIAS_SCRIPT_PATH}"
+}
+
+# Function to create systemd service file for the script
+create_systemd_service() {
+    echo "Creating systemd service for publish_alias.sh"
+
+    local service_path="/etc/systemd/system/publish-alias.service"
+    sudo tee "${service_path}" >/dev/null <<EOF
+[Unit]
+Description=Publish Alias Hostnames
+After=avahi-daemon.service
+Wants=avahi-daemon.service
+
+[Service]
+Type=oneshot
+ExecStart=${ALIAS_SCRIPT_PATH}
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    # Reload the systemd manager configuration
+    sudo systemctl daemon-reload
+
+    # Enable the service
+    sudo systemctl enable publish-alias.service
+
+    # Start the service
+    sudo systemctl start publish-alias.service
 }
 
 # Function to create .env file with default environment variables
 create_env_file() {
     echo "Creating .env file with default environment variables"
 
-    cat > .env <<EOF
+    cat >.env <<EOF
 # Environment variables for docker-compose
 POSTGRES_DB=webdb
 POSTGRES_USER=admin
@@ -66,13 +104,7 @@ create_env_file
 # Create and setup the alias hostname script
 create_alias_script
 
-# Add the alias hostname script to crontab for startup execution
-echo "Adding alias hostname script to crontab for startup execution"
-CRON_JOB="@reboot $(pwd)/publish_alias.sh"
-(crontab -l 2>/dev/null | grep -v "$(pwd)/publish_alias.sh"; echo "$CRON_JOB") | crontab -
-
-# Run the alias hostname script
-echo "Running alias hostname script"
-./publish_alias.sh
+# Create and setup the systemd service
+create_systemd_service
 
 echo "Raspberry Pi setup completed."
