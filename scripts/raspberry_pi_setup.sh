9f5170a6921666c631173203eb7dd6878becb6ae
Nate Brandeburg <brandeburgnate@gmail.com>
2023-12-13 22:55:28 -0600

    Add headless mode and force mode options to raspberry_pi_setup.sh

--- scripts/raspberry_pi_setup.sh
+++ scripts/raspberry_pi_setup.sh
@@ -5,28 +5,67 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Check if we are running in headless mode
+headless_mode=0
+force_mode=0
+
+# Check for --headless argument
+for arg in "$@"
+do
+    case $arg in
+        --headless)
+            headless_mode=1
+            ;;
+        -f|--force)
+            force_mode=1
+            ;;
+    esac
+done
+
 # Check if Avahi is installed
-if ! dpkg -s avahi-daemon avahi-utils >/dev/null 2>&1; then
+if ! dpkg -s avahi-daemon avahi-utils >/dev/null 2>&1 || [ $force_mode -eq 1 ]; then
     echo "Installing Avahi..."
     sudo apt-get update
-    sudo apt-get install -y avahi-daemon avahi-utils
+    sudo apt-get install -y avahi-daemon avahi-utils || { echo "Failed to install Avahi"; exit 1; }
 else
     echo "Avahi already installed."
 fi
 
-# Enable and start Avahi service
-echo "Enabling and starting Avahi service"
-sudo systemctl enable avahi-daemon
-sudo systemctl start avahi-daemon
-echo "Avahi service enabled and started."
+# Check if Avahi service is enabled and running
+if ! systemctl is-active --quiet avahi-daemon || [ $force_mode -eq 1 ]; then
+    echo "Enabling and starting Avahi service"
+    sudo systemctl enable avahi-daemon
+    sudo systemctl start avahi-daemon
+    echo "Avahi service enabled and started."
+else
+    echo "Avahi service is already running."
+fi
 
 # Path for the alias hostname script
 ALIAS_SCRIPT_PATH="/usr/local/bin/publish_alias.sh"
 
 # Function to create alias hostname script
 create_alias_script() {
-    echo "Creating alias hostname script at ${ALIAS_SCRIPT_PATH}"
+    if [ -f "${ALIAS_SCRIPT_PATH}" ] && [ -x "${ALIAS_SCRIPT_PATH}" ]; then
+        if [ $force_mode -eq 1 ]; then
+            echo "Force mode: Forcing creation of alias hostname script."
+        else
+            if [ $headless_mode -eq 1 ]; then
+                echo "Headless mode: Alias hostname script already exists, skipping creation."
+                return
+            fi
 
+            read -p "Alias hostname script already exists. Do you want to overwrite it? (y/N): " confirm
+            if [[ ! $confirm =~ ^[Yy]$ ]]; then
+                echo "Aborting alias hostname script creation."
+                return
+            fi
+        fi
+    else
+        echo "Alias hostname script does not exist or is not executable. Creating script."
+    fi
+
+    echo "Creating alias hostname script at ${ALIAS_SCRIPT_PATH}"
     sudo tee "${ALIAS_SCRIPT_PATH}" >/dev/null <<'EOF'
 #!/bin/bash
 IP_ADDRESS=$(hostname -I | cut -d' ' -f1)
@@ -51,9 +90,31 @@ EOF
 
 # Function to create systemd service file for the script
 create_systemd_service() {
+    local service_path="/etc/systemd/system/publish-alias.service"
+
+    if [ -f "${service_path}" ]; then
+        if [ $force_mode -eq 1 ]; then
+            echo "Force mode: Forcing creation of systemd service."
+        else
+            if [ -f "${service_path}" ]; then
+                if [ $headless_mode -eq 1 ]; then
+                    echo "Headless mode: Systemd service for publish_alias.sh already exists, skipping creation."
+                    return
+                fi
+
+                read -p "Systemd service for publish_alias.sh already exists. Do you want to overwrite it? (y/N): " confirm
+                if [[ ! $confirm =~ ^[Yy]$ ]]; then
+                    echo "Aborting systemd service creation."
+                    return
+                fi
+            fi
+        fi
+    else
+        echo "Systemd service for publish_alias.sh does not exist. Creating service."
+    fi
+
     echo "Creating systemd service for publish_alias.sh"
 
-    local service_path="/etc/systemd/system/publish-alias.service"
     sudo tee "${service_path}" >/dev/null <<EOF
 [Unit]
 Description=Publish Alias Hostnames
@@ -81,6 +142,25 @@ EOF
 
 # Function to create .env file with default environment variables
 create_env_file() {
+    if [ -f ".env" ]; then
+        if [ $force_mode -eq 1 ]; then
+            echo "Force mode: Forcing creation of .env file."
+        else
+            if [ $headless_mode -eq 1 ]; then
+                echo "Headless mode: .env file already exists, skipping creation."
+                return
+            fi
+
+            read -p ".env file already exists. Do you want to overwrite it? (y/N): " confirm
+            if [[ ! $confirm =~ ^[Yy]$ ]]; then
+                echo "Aborting .env file creation."
+                return
+            fi
+        fi
+    else
+        echo ".env file does not exist. Creating file."
+    fi
+
     echo "Creating .env file with default environment variables"
 
     cat >.env <<EOF
@@ -96,6 +176,12 @@ OCTOPRINT_DOMAIN=octoprint.local
 TRAEFIK_DOMAIN=traefik.local
 ADMIN_PASSWORD=changeme!
 EOF
+    echo -e "\033[1;33m.env file created/overwritten.\033[0m"
+    echo -e "\033[1;32mIMPORTANT:\033[0m Please modify the following variables to unique values of your choosing:"
+    echo -e "\033[1;34mPOSTGRES_USER\033[0m"
+    echo -e "\033[1;34mPOSTGRES_PASSWORD\033[0m"
+    echo -e "\033[1;34mSECRET_KEY\033[0m"
+    echo -e "\033[1;34mADMIN_PASSWORD\033[0m"
 }
 
 # Create the .env file
